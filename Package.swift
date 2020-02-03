1f96fb16b677f0bdd0d66ce04768b1661a4788e2
Benjamin Davis <ben@pinwheeldevinc.com>
2020-02-03 12:07:16 -0600

    abcd

--- Package.swift
+++ Package.swift
@@ -1,12 +1,23 @@
-//
-//  Package.swift
-//  CommunicatorExample
-//
-//  Created by Robert Davis on 2/3/20.
-//  Copyright © 2020 Jason Ji. All rights reserved.
-//
-
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+let package = Package(
+    name: "CommunicatorKit",
+    platforms: [
+        .iOS(.v8), .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "CommunicatorKit", targets: ["CommunicatorKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rbenjamin/CommunicatorKit", from: "1.0"),
+dsfdf    ],
+    targets: [
+        .target(
+            name: "CommunicatorKit",
+            dependencies: [],
+            path: "CommunicatorKit"),
 
-rear
+    ]
+)
